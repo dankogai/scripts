740fb7576d3a573b97e3d059f1144199c3e34832
teramako <teramako@gmail.com>
2011-12-07 21:47:09 +0900

    update

--- shell/cat.sh
+++ shell/cat.sh
@@ -8,10 +8,13 @@
 # show AA of a cat if non-stdin or not specified 2 or more files
 # 標準入力が無い、または、2つ以上のファイルが指定されない場合、猫のAAを流します
 #
+# @see http://kenji1234.blog75.fc2.com/blog-entry-19.html
+#
 
 concatable () {
+  local MIN_FILE_COUNT=2
   if [ ! -t 0 ];then
-    return 0
+    MIN_FILE_COUNT=1
   fi
   local FILE_COUNT=0
   for arg in "$@"; do
@@ -19,7 +22,7 @@ concatable () {
       (( FILE_COUNT++ ))
     fi
   done
-  if [ $FILE_COUNT -gt 1 ]; then
+  if (( FILE_COUNT >= MIN_FILE_COUNT )); then
     return 0
   fi
   return 1
@@ -30,23 +33,53 @@ if concatable "$@" ; then
   exit $?
 fi
 
-
-CAT_LENGTH=85
+CAT_LENGTH=80
 CAT_HEIGHT=13
-CAT[0]='                    lヽ、                   /ヽ'
-CAT[1]="                    i!  ﾞヽ、            ／   ﾞi!           ..,,  .,,.\;\;\'\'⌒ヽ"
-CAT[2]="                    l       ゝ-─‐-／\'       i!   , ,__,,;\'\"   \"\';       ,ﾉ\""
-CAT[3]="                  ,／\"                        i!\'\'\"   ....ﾞ\'\'\;\;..,,\;\;    ,,Y\""
-CAT[4]="                ,/\'                           〈                  \'i\;\;- ､,,    "
-CAT[5]="                i\'                             \'i,                         ﾞ\"ヽ、  "
-CAT[6]="                i!  ●          ●       ＊ ,\'i                             ﾞ）"
-CAT[7]="                \'i,:::    ﾄ─‐ｲ      :::::::    ,/        \'          ﾞ\",;\'\'i,-‐\'\""
-CAT[8]="          ,,-‐\'\'\"ヽ､    ヽ,_ノ         ,,-‐                  ,..\;\;\;ﾞ\""
-CAT[9]="        （        ,,, \'\'          ,,.-‐\'\'\"              ,,\'\"´｀\'´"
-CAT[10]="         ヽ,..-‐\'\'       ,.-‐\'\'\"            ノ-‐\'\'\"´"
-CAT[11]="                      （             ,. -\'\""
-CAT[12]="                         ヽ、,,.. -‐\'\'\'\""
+ CAT[0]="            lヽ、                  /ヽ"
+ CAT[1]="            i!  ﾞヽ、           ／   ﾞi!           ..,,  .,,.;;''⌒ヽ"
+ CAT[2]="            l       ゝ-─‐-／'       i!   , ,__,,;'\"   \"';       ,ﾉ\""
+ CAT[3]="          ,／\"                        i!''\"   ....ﾞ'';;..,,;;    ,,Y\""
+ CAT[4]="        ,/'                           〈                  'i;;- ､,,    "
+ CAT[5]="        i'                             'i,                         ﾞ\"ヽ、"
+ CAT[6]="        i!   ●            ●      ＊ ,'i                            ﾞ )"
+ CAT[7]="        'i,:::    ﾄ----ｲ      :::::::    ,/        '          ﾞ\",;''i,--'\""
+ CAT[8]="  ,,-‐''\"\\､       \\,_/          ,,-‐                  ,..;;;ﾞ\""
+ CAT[9]="（        ,,, ''          ,,.-‐''\"              ,,'\"´｀'´"
+CAT[10]=" ヽ,..-‐''       ,.-‐''\"             /-‐''\"´"
+CAT[11]="              （             ,. -'\""
+CAT[12]="                 ヽ、,,.. -‐'''\""
 
+CAT_2_LEIGTH=46
+CAT_2_HEIGHT=10
+CAT_2[0]="       ;' ':;,,          ,;'':;,"
+CAT_2[1]="      ;'     ':;,.,.,.,.,.,,,;'    ';,     っ"
+CAT_2[2]="    ,:'                     : :､"
+CAT_2[3]="  ,:'ノ            ＼     ::::::::',     っ"
+CAT_2[4]=" :'   ●         ●         :::::i."
+CAT_2[5]=" i  ///(_人＿) //// *  :::::i    "
+CAT_2[6]=" ,i.       し'                :::::i"
+CAT_2[7]="(⌒)              (⌒) ::::::::: /" 
+CAT_2[8]=" ;,  :'              ; : : ::::::::｀:､"
+CAT_2[9]="  ; ,:'               ';,.  : : ::::::｀:､"
+
+START=0
+set_START () {
+  END=0
+  ELAPS=0
+  local dummy
+  read START dummy < /proc/uptime
+}
+set_ELAPS () {
+  local dummy
+  read END dummy < /proc/uptime
+  ELAPS=$(( ${END/./}0-${START/./}0 ))
+}
+mili_sleep () {
+    set_START
+    while (( ELAPS <= $1 )); do
+      set_ELAPS
+    done
+}
 
 clr() {
   echo -e '\033[H\033[2J\c'
@@ -75,8 +108,6 @@ show_cat() {
   local y=$2
   local start=0
   local length=$(( COLUMNS - $1 ))
-  local p1=$(( $3 % 2 ))
-  local p2=$(( $3 % 6 ))
   (( x < 0 )) && start=$(( - x )) x=0
   for (( i=0; i < CAT_HEIGHT; i++ ));do
     cursor $x $(( y++ ))
@@ -84,25 +115,36 @@ show_cat() {
   done
 }
 
+show_cat_2 () {
+  local x=$1
+  local y=$2
+  for (( i=0; i < CAT_2_HEIGHT; i++ ));do
+    cursor $x $(( y++ ))
+    echo "${CAT_2[$i]}"
+  done
+}
+
 #eval `resize`
 COLUMNS=`tput cols`
 LINES=`tput lines`
+trap '' 2
 new_screen
 clr
-#h=$(( (LINES - STEAM_HEIGHT - D51STR_HEIGHT - D51WHL_HEIGHT) / 2 ))
 h=$(( (LINES - CAT_HEIGHT) / 2 ))
 c=0;
-# while (( c < COLUMNS + SL_LENGTH )); do
 while (( c < COLUMNS + CAT_LENGTH )); do
   x=$(( COLUMNS - c ))
   show_cat $x $h $c
-  for (( i=0; i<100; i++));do
-    echo "" >/dev/null
-  done
-  #sleep 0.01
-  c=$(( c + 1 ))
+  mili_sleep 20
+  c=$(( c + 2 ))
   clr
 done
+
+show_cat_2 20 $h
+cursor 20 $(( $h + 13 ))
+echo "cat はきちんと concat として使いましょう"
+sleep 4
+clr
 exit_screen
 
 # vim: sw=2 ts=2 et fdm=marker:
